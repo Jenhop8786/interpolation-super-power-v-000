17476cf0804983e98a1452cca1997315e3105e63
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-03 22:55:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
-rainbow = ["R:red", "O:orange", "Y:yellow", "G:green", "B:blue", "I:indigo", "V:violet"]
+rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 
 def display_rainbow(rainbow)
-  puts "Colors of the #{rainbow}"
+  puts "#{rainbow}"
 end
