23430c1b8b2af4badfc5a8aebe38d76db4bb10da
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-03 23:28:58 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,5 @@ colors = "red", "orange", "yellow", "green", "blue", "indigo", "violet"
 
 
 def display_rainbow(colors)
-  puts "display_rainbow #{colors}"
+  puts "display_rainbow #{colors[0]}"
 end
